952b5c92c87cb888f537c929b3db104db52f92b2
saarakhalid <khalidsaara004@gmail.com>
2023-12-25 02:28:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+// find matching
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+
+  //Fuzzy match
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+
+  //match name
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+
+  const drivers = [
+    { name: "John", hometown: "New York" },
+    { name: "Jane", hometown: "Los Angeles" },
+
+  ];
+  
+  const matchingDrivers = findMatching(["John", "Jane", "Bob"], "john");
+  console.log(matchingDrivers); 
+  
+  const fuzzyMatchingDrivers = fuzzyMatch(["John", "Jane", "Bob"], "j");
+  console.log(fuzzyMatchingDrivers); 
+  
+  const matchedByName = matchName(drivers, "John");
+  console.log(matchedByName); 
+  
